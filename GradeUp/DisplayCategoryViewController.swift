f209aecb1bbeb1cd70c3c3a5efd84e6fa7567733
Adrien morel <adrien.morel@me.com>
2016-02-20 11:50:36 -0800

    compute color of label from success rate

--- GradeUp/DisplayCategoryViewController.swift
+++ GradeUp/DisplayCategoryViewController.swift
@@ -20,9 +20,20 @@ class DisplayCategoryViewController: UIViewController {
     @IBOutlet weak var bestSessionLabel: UILabel!
     @IBOutlet weak var numberOfQuestionLabel: UILabel!
     
+    func computeComposentValue(percentage: Float, greenVal: Float, redVal: Float) -> CGFloat {
+        return CGFloat(
+            (greenVal * percentage + redVal * (1.0 - percentage)) / (percentage + (1.0 - percentage))
+        )
+    }
+    
     func computeColor(percentage: Int) -> UIColor {
         
-        return UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+        let percentage: Float = Float(percentage) / 100
+        
+        return UIColor(
+            red:   computeComposentValue(percentage, greenVal: 0, redVal: 0.392),
+            green: computeComposentValue(percentage, greenVal: 0.392, redVal: 0),
+            blue:  computeComposentValue(percentage, greenVal: 0.138, redVal: 0.011), alpha: 1)
     }
     
     func setLabelColorFromScore() {
